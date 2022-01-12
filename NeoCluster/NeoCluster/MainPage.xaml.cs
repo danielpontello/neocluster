9820a98b888fda431f920994868941933e875f4d
Daniel Sader <danielpontello@gmail.com>
2022-01-12 23:36:24 +0000

    Changed project type to UWP to make it easier to get device sensors

--- NeoCluster/NeoCluster/MainPage.xaml.cs
+++ NeoCluster/NeoCluster/MainPage.xaml.cs
@@ -1,39 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
-using System.Text;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
+using Windows.Networking.Sockets;
+using Windows.UI.Popups;
+
+// O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x416
 
 namespace NeoCluster
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
     /// </summary>
-    public partial class MainWindow : Window
+    public sealed partial class MainPage : Page
     {
-        public MainWindow()
+        private int packet = 0;
+        public MainPage()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
 
-        private void btnDebug_Click(object sender, RoutedEventArgs e)
+        private async void btnDebug_Click(object sender, RoutedEventArgs e)
         {
-            Task.Run(async () => {
+            lblDebug.Text = "Started";
+
+            await Task.Run(async () => {
                 using (var Client = new UdpClient(5678))
                 {
+                    int packet = 0;
+
                     while (true)
                     {
                         var ReceivedData = await Client.ReceiveAsync();
+                        packet += 1;
 
                         byte[] data = ReceivedData.Buffer;
 
@@ -51,10 +63,12 @@ namespace NeoCluster
                         }
                         int rSpeed = (int)speed;
 
-                        Dispatcher.Invoke(() => {
+                        await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => {
                             lblGear.Text = "" + gear;
                             lblSpeed.Text = "" + rSpeed;
                             prgRpm.Value = barRpm;
+
+                            lblDebug.Text = "Packets received: " + packet;
                         });
                     }
                 }
