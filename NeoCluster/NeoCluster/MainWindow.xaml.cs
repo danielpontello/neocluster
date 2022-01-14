954fe0ec0a5efb659d51451a3cb2ae000f4bc0e0
Daniel Sader <danielpontello@gmail.com>
2022-01-14 00:40:27 +0000

    More UI changes and data added

--- NeoCluster/NeoCluster/MainWindow.xaml.cs
+++ NeoCluster/NeoCluster/MainWindow.xaml.cs
@@ -22,9 +22,14 @@ namespace NeoCluster
     /// </summary>
     public partial class MainWindow : Window
     {
+        TextBlock[] gears;
+        SolidColorBrush disabledBrush = new SolidColorBrush(Color.FromRgb(130, 130, 130));
+        SolidColorBrush enabledBrush = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+
         public MainWindow()
         {
             InitializeComponent();
+            gears = new TextBlock[] { lblGR, lblG1, lblG2, lblG3, lblG4, lblG5, lblG6, lblG7 };
         }
 
         private void btnStart_Click(object sender, RoutedEventArgs e)
@@ -38,6 +43,8 @@ namespace NeoCluster
 
                         byte[] data = ReceivedData.Buffer;
 
+                        bool raceOn = (data[0] > 0);
+
                         float maxRpm = BitConverter.ToSingle(data, 8);
                         float idleRpm = BitConverter.ToSingle(data, 12);
                         float currentRpm = BitConverter.ToSingle(data, 16);
@@ -49,6 +56,15 @@ namespace NeoCluster
                         float speed = BitConverter.ToSingle(data, 256) * 3.6f;
                         float barSpeed = (speed / 400.0f) * 100.0f;
                         int gear = data[319];
+                        int clampedGear = Math.Clamp(gear, 0, gears.Length-1);
+
+                        float bestLapTime = BitConverter.ToSingle(data, 296);
+                        float lastLapTime = BitConverter.ToSingle(data, 300);
+                        float currentLapTime = BitConverter.ToSingle(data, 304);
+                        float currentRaceTime = BitConverter.ToSingle(data, 308);
+
+                        byte lap = data[312];
+                        byte position = data[314];
 
                         float barRpm = 0;
                         if (maxRpm > 0)
@@ -57,12 +73,32 @@ namespace NeoCluster
                         }
                         int rSpeed = (int)speed;
 
+                        TimeSpan currentLapTimespan = TimeSpan.FromSeconds((double)(new decimal(currentLapTime)));
+                        TimeSpan bestLapTimespan = TimeSpan.FromSeconds((double)(new decimal(currentLapTime)));
+                        TimeSpan currentRaceTimespan = TimeSpan.FromSeconds((double)(new decimal(currentRaceTime)));
+
                         Dispatcher.Invoke(() => {
-                            lblGear.Text = "" + gear;
+                            for(int i=0; i<gears.Length; i++)
+                            {
+                                gears[i].Foreground = disabledBrush;
+                            }
+                            gears[clampedGear].Foreground = enabledBrush;
+
                             lblSpeed.Text = "" + rSpeed;
-                            // lblDebug.Text = "x: " + x + " y: " + y + " z: " + z;
+                            lblDebug.Text = "" + gear;
                             prgRpm.Value = barRpm;
                             prgSpeed.Value = barSpeed;
+
+                            lblBestLap.Text = bestLapTimespan.ToString(@"mm\:ss\:fff");
+                            lblCurrentLap.Text = currentLapTimespan.ToString(@"mm\:ss\:fff");
+                            lblRaceTime.Text = currentRaceTimespan.ToString(@"mm\:ss\:fff");
+
+                            lblPosition.Text = position + "/12";
+                            lblLaps.Text = lap + "/3";
+                            lblClock.Text = DateTime.Now.ToString("g");
+
+                            lblConnected.Text = "Online";
+                            lblConnected.Foreground = enabledBrush;
                         });
                     }
                 }
